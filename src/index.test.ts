aef0922d6a087535e2d6413fd36c2562c22522a5
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-04-18 15:05:24 +0100

    Feat: Created Checkout class with pricingRules

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,66 @@
 import { add } from "./index";
+import { Checkout, princingRules } from "./index";
 
 describe("test the add function", () => {
   test("should add two numbers", () => {
     expect(add(4, 3)).toBe(7);
+  });
+});
+
+describe("test the checkout class", () => {
+  test("should return the right cost for any amount of coffee", () => {
+    const checkout = new Checkout(princingRules);
+    checkout.scan("CF1");
+    expect(checkout.total()).toBe(11.23);
+    checkout.scan("CF1");
+    expect(checkout.total()).toBe(22.46);
+    checkout.scan("CF1");
+    checkout.scan("CF1");
+    checkout.scan("CF1");
+    expect(checkout.total()).toBe(56.15);
+  });
+
+  test("should return the right cost for any amount of fruit tea", () => {
+    const checkout = new Checkout(princingRules);
+    checkout.scan("FR1");
+    expect(checkout.total()).toBe(3.11);
+    checkout.scan("FR1");
+    expect(checkout.total()).toBe(3.11);
+    checkout.scan("FR1");
+    expect(checkout.total()).toBe(6.22);
+    checkout.scan("FR1");
+    expect(checkout.total()).toBe(6.22);
+    checkout.scan("FR1");
+    expect(checkout.total()).toBe(9.33);
+  });
+
+  test("should return the right cost for any amount of strawberries", () => {
+    const checkout = new Checkout(princingRules);
+    checkout.scan("SR1");
+    expect(checkout.total()).toBe(5);
+    checkout.scan("SR1");
+    expect(checkout.total()).toBe(10);
+    checkout.scan("SR1");
+    expect(checkout.total()).toBe(13.5);
+    checkout.scan("SR1");
+    expect(checkout.total()).toBe(18);
+    checkout.scan("SR1");
+    expect(checkout.total()).toBe(22.5);
+  });
+
+  test("should return the correct cost for a mix of products", () => {
+    const checkout = new Checkout(princingRules);
+    const items = ["FR1", "SR1", "FR1", "FR1", "CF1"];
+    items.forEach((item) => {
+      checkout.scan(item);
+    });
+    expect(checkout.total()).toBe(22.45);
+
+    const checkout2 = new Checkout(princingRules);
+    const items2 = ["SR1", "SR1", "FR1", "SR1"];
+    items2.forEach((item) => {
+      checkout2.scan(item);
+    });
+    expect(checkout2.total()).toBe(16.61);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,57 @@
 export const add = (x: number, y: number): number => {
   return x + y;
 };
+
+export class Checkout {
+  pricingRules: Function;
+  items: string[] = [];
+  constructor(princingRules: Function) {
+    this.pricingRules = princingRules;
+  }
+
+  scan(item: string) {
+    this.items.push(item);
+  }
+
+  total() {
+    return calculateTotal(this.items);
+  }
+}
+
+type Quantities = {
+  [key: string]: number;
+};
+
+function calculateTotal(items: string[]) {
+  const quantities: Quantities = {};
+  items.forEach((item) => {
+    quantities[item] = items.filter((i) => item === i).length;
+  });
+
+  return princingRules(quantities) / 100;
+}
+
+export function princingRules(quantities: Quantities): number {
+  let cost = 0;
+  if (quantities.hasOwnProperty("CF1")) {
+    cost += quantities.CF1 * 1123;
+  }
+
+  if (quantities.hasOwnProperty("FR1")) {
+    if (quantities.FR1 % 2 === 0) {
+      cost += (quantities.FR1 * 311) / 2;
+    } else {
+      cost += ((quantities.FR1 + 1) / 2) * 311;
+    }
+  }
+
+  if (quantities.hasOwnProperty("SR1")) {
+    if (quantities.SR1 >= 3) {
+      cost += quantities.SR1 * 450;
+    } else {
+      cost += quantities.SR1 * 500;
+    }
+  }
+
+  return cost;
+}
